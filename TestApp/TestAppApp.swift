d51fdafad228c3d0d1e5e800b77970ed818733ff
westsider <whansen1@mac.com>
2023-11-09 15:27:24 -0600

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Warren Hansen on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
